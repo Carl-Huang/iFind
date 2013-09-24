20d269c759399228627354a82703e2021d6fa235
vedon <vedon.fu@gmail.com>
2013-09-24 16:18:59 +0800

    sqlmng

--- iFind/DeviceDetailViewController.h
+++ iFind/DeviceDetailViewController.h
@@ -24,7 +24,6 @@ typedef void (^DidSelectTableviewRowCongigureBlock) (id item);
 @class FPPopoverController;
 @class PhotoManager;
 @class SQLManager;
-
 @interface DeviceDetailViewController : UIViewController
 {
     PopUpTableViewController *popUpTableviewController;
